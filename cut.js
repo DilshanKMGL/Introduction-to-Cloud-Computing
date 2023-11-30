d6324ef9568f66eb34f02e602f77e59254661b6d
Lahiru Dilshan <dilshankmg@gmail.com>
2023-11-30 09:02:28 +0530

    Delete cut.js

--- cut.js
+++
@@ -1,9 +0,0 @@
-var explorer = cdm("explorer");
-var geometry = explorer.geometry();
-var topology = explorer.topology();
-var axes = explorer.geometry().child("referenceHolder/axisHolder");
-var planes = explorer.geometry().child("referenceHolder/planeHolder");
-var box_1 = geometry.box(1, 1, 1);
-var cylinder_1 = geometry.cylinder(1, 1, 0, 0, 1);
-var cut_1 = geometry.cut([box_1], [cylinder_1]);
-cut_1.translate(2, 0, 0);
